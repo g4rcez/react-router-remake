93fb06a76d58c4bc57f366519a8a7dd65d6487a9
Leonardo de Rezende Avelino <leonardo.avelino16@hotmail.com>
2024-03-26 16:14:24 -0300

    improve: use Link and routes.links

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,3 +1,11 @@
+import { routes } from "../links";
+import { Link } from "../router/link";
+
 export default function IndexPage() {
-    return <h1>Eu sou a página index</h1>
+  return (
+    <section>
+      <h1>Eu sou a página index</h1>
+      <Link href={routes.links.code}>Ir para Code Challenge</Link>
+    </section>
+  );
 }

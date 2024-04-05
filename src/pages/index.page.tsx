819684dc1de23db7c0456ef9055963c825d5b4eb
Elivelton Rodrigues <elivelton.lima@stone.com.br>
2024-04-05 15:11:57 -0300

    feat: adiciona add case
    Ao clicar num link que aponta para o path atual, nada acontece.

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -7,6 +7,7 @@ export default function IndexPage() {
       <h1>Eu sou a página index</h1>
       <Link href={routes.links.code}>Ir para Code Challenge</Link>
       <Link href="https://www.google.com.br">Ir para o Google</Link>
+      <Link href={routes.links.index}>Permanecer na página</Link>
     </section>
   );
 }
--- src/router/link.tsx
+++ src/router/link.tsx
@@ -1,23 +1,19 @@
 import { AnchorHTMLAttributes, MouseEvent } from "react";
-import { useHistory } from "./hook.ts";
+import { useHistory, useLocation } from "./hook.ts";
 
 export type LinkProps = AnchorHTMLAttributes<HTMLAnchorElement>;
-
-/**
- * [ x ] adicionar comportamento de SPA;
- * [  ] verificar se o link aponta para o pathname atual;
- * [ x ] add case -> link externo;
- * */
 
 const isExternalLink = (href: string) => /https?:\/\//.test(href);
 
 export const Link = (props: LinkProps) => {
   const history = useHistory();
+  const location = useLocation();
 
   const onClick = (e: MouseEvent<HTMLAnchorElement> | undefined) => {
     if (!props.href) return;
     if (isExternalLink(props.href)) return;
     e?.preventDefault();
+    if (location.pathname === props.href) return;
     history.push(props.href);
   };
 
